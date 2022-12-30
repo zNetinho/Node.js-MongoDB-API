839aa9f71fe01c4c2e6ee4342c3fa74e7f5da4ca
Neto Flavio <95658733+zNetinho@users.noreply.github.com>
2022-12-29 22:08:53 -0300

    :sparkles: [FEAT] post

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -6,6 +6,7 @@ import { msgPadrao } from '../../types/msgPadrao';
 import { conectMongoDB } from '../../middlewares/conectionDB';
 import { JWTvalidator } from '../../middlewares/validatorJWT';
 import { corsRules } from '../../middlewares/corsRules';
+import { followingModel } from './../../models/followersModels';
 
 const feedsUser = async (req: NextApiRequest, res: NextApiResponse<msgPadrao | any>) => {
     try {
@@ -19,8 +20,42 @@ const feedsUser = async (req: NextApiRequest, res: NextApiResponse<msgPadrao | a
                     .find({ idUser: user._id })
                     .sort({ data: -1 })
                 return res.status(200).json(posts)
-            }
+            }    
+            
         }
+        else {
+            const userId = req?.query;
+            const userLogged = await userModel.findById(userId)
+            if(!userLogged) {
+                return res.status(400).json({ error: 'User not find'})
+            }
+
+            const followers = await followingModel.find({userId: userLogged._id});
+            const followersId = followers.map(s => s.userIdfolloweds);
+
+            const posts = await postModel.find({
+                $or: [
+                    {userId: userLogged._id},
+                    {userId: followersId}
+                ]
+            })
+            .sort({data: -1})
+
+            const result = [];
+            for(const post of posts) {
+                const userPost = await postModel.findById(post.userId)
+                if(userPost){
+                    const final = {...post._doc, user : {
+                        name: userPost?.name,
+                        avatar: userPost?.avatar
+                    }};
+                    result.push(final)
+                }
+            }
+            return res.status(200).json(result)
+        }
+        return res.status(405).json({error: 'Method not allowed'})
+
     } catch (error) {
         console.log(error)
     }
