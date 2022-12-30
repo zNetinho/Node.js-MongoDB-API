c2c19ef91a1f6678307f0d6793972a622f8119e4
Neto Flavio <95658733+zNetinho@users.noreply.github.com>
2022-12-29 22:23:53 -0300

    []

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -1,65 +1,68 @@
-import { postModel } from './../../models/postModels';
-import { NextApiRequest } from 'next';
-import { userModel } from './../../models/userModels';
-import { NextApiResponse } from 'next';
-import { msgPadrao } from '../../types/msgPadrao';
-import { conectMongoDB } from '../../middlewares/conectionDB';
-import { JWTvalidator } from '../../middlewares/validatorJWT';
-import { corsRules } from '../../middlewares/corsRules';
-import { followingModel } from './../../models/followersModels';
+import { postModel } from "./../../models/postModels";
+import { NextApiRequest } from "next";
+import { userModel } from "./../../models/userModels";
+import { NextApiResponse } from "next";
+import { msgPadrao } from "../../types/msgPadrao";
+import { conectMongoDB } from "../../middlewares/conectionDB";
+import { JWTvalidator } from "../../middlewares/validatorJWT";
+import { corsRules } from "../../middlewares/corsRules";
+import { followingModel } from "./../../models/followersModels";
 
-const feedsUser = async (req: NextApiRequest, res: NextApiResponse<msgPadrao | any>) => {
-    try {
-        if (req.method === 'GET') {
-            if (req?.query?.id) {
-                const user = await userModel.findById(req?.query?.id);
-                if (!user) {
-                    return res.status(400).json({ error: 'user not exist' });
-                }
-                const posts = await postModel
-                    .find({ idUser: user._id })
-                    .sort({ data: -1 })
-                return res.status(200).json(posts)
-            }    
-            
+const feedsUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse<msgPadrao | any>
+) => {
+  try {
+    if (req.method === "GET") {
+      if (req?.query?.id) {
+        const user = await userModel.findById(req?.query?.id);
+        if (!user) {
+          return res.status(400).json({ error: "user not exist" });
         }
-        else {
-            const userId = req?.query;
-            const userLogged = await userModel.findById(userId)
-            if(!userLogged) {
-                return res.status(400).json({ error: 'User not find'})
-            }
+        const posts = await postModel
+          .find({ idUser: user._id })
+          .sort({ data: -1 });
+        return res.status(200).json(posts);
+      }
+     else {
+      const {userId} = req?.query;
+      const userLogged = await userModel.findById(userId);
+      if (!userLogged) {
+        return res.status(400).json({ error: "User not find" });
+      }
 
-            const followers = await followingModel.find({userId: userLogged._id});
-            const followersId = followers.map(s => s.userIdfolloweds);
+      const followers = await followingModel.find({ userId: userLogged._id });
+      const followersId = followers.map((s) => s.userIdfolloweds);
+      console.log(userLogged)
 
-            const posts = await postModel.find({
-                $or: [
-                    {userId: userLogged._id},
-                    {userId: followersId}
-                ]
-            })
-            .sort({data: -1})
+      const posts = await postModel
+        .find({
+          $or: [{ userId: userLogged._id }, { userId: followersId }],
+        })
+        .sort({ data: -1 });
 
-            const result = [];
-            for(const post of posts) {
-                const userPost = await postModel.findById(post.userId)
-                if(userPost){
-                    const final = {...post._doc, user : {
-                        name: userPost?.name,
-                        avatar: userPost?.avatar
-                    }};
-                    result.push(final)
-                }
-            }
-            return res.status(200).json(result)
+      const result = [];
+      for (const post of posts) {
+        const userPost = await postModel.findById(post.idUser);
+        if (userPost) {
+          const final = {
+            ...post._doc,
+            user: {
+              name: userPost?.name,
+              avatar: userPost?.avatar,
+            },
+          };
+          result.push(final);
         }
-        return res.status(405).json({error: 'Method not allowed'})
-
-    } catch (error) {
-        console.log(error)
+      }
+      return res.status(200).json(result);
     }
-    return res.status(400).json({ error: 'not possible obtain feed' })
 }
+    return res.status(405).json({ error: "Method not allowed" });
+  } catch (error) {
+    console.log(error);
+  }
+  return res.status(400).json({ error: "not possible obtain feed" });
+};
 
-export default corsRules(JWTvalidator(conectMongoDB(feedsUser)))
+export default corsRules(JWTvalidator(conectMongoDB(feedsUser)));
